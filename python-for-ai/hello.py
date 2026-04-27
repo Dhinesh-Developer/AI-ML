467c6d65d9c466794c7815598bb6bccf33ba423c
Dhinesh-Developer <dhineshdeveloper07@gmail.com>
2026-04-27 20:49:58 +0530

    Basics of python learning

---
+++ python-for-ai/hello.py
@@ -0,0 +1,14 @@
+print("Hello ")
+
+name = "Alice"
+print(name)
+
+age = 20
+print(age)
+
+is_student = True
+print(is_student)
+
+first_name = "dhinesh"
+last_name = "kumar"
+print(first_name +" "+last_name)
---
+++ python-for-ai/loops.py
@@ -0,0 +1,16 @@
+
+for i in range(5):
+    print(i)
+
+for i in range(1,6):
+    print(i)
+
+for i in range(0,10,2):
+    print(i)    
+
+
+# loop through text
+
+name = "python"
+for letter in name:
+    print(letter)    
